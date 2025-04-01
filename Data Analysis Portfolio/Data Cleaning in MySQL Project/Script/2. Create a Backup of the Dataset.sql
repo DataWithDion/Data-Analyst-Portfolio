8622874ef76d447d8f166ecae0de55b9c44810bd
Dion <dionhasolli30@gmail.com>
2025-04-02 01:32:37 +0200

    Delete Data Analysis Portfolio directory

--- Data Analysis Portfolio/Data Cleaning in MySQL Project/Script/2. Create a Backup of the Dataset.sql
+++
@@ -1,4 +0,0 @@
--- 2. Create a Backup of the Dataset
--- This ensures that we can always revert to the original data in case of errors
-CREATE TABLE dirty_cafe_sales_backup AS 
-SELECT * FROM dirty_cafe_sales;
--- Data Analysis Portfolio/Data Cleaning in MySQL Project/Script/3. Standardize Column Names.sql
+++
@@ -1,7 +0,0 @@
--- 3. Standardize Column Names
--- Rename columns to have a consistent naming convention
-ALTER TABLE dirty_cafe_sales RENAME COLUMN `Transaction ID` TO `Transaction_ID`;
-ALTER TABLE dirty_cafe_sales RENAME COLUMN `Price Per Unit` TO `Price_Per_Unit`;
-ALTER TABLE dirty_cafe_sales RENAME COLUMN `Total Spent` TO `Total_Spent`;
-ALTER TABLE dirty_cafe_sales RENAME COLUMN `Payment Method` TO `Payment_Method`;
-ALTER TABLE dirty_cafe_sales RENAME COLUMN `Transaction Date` TO `Transaction_Date`;
